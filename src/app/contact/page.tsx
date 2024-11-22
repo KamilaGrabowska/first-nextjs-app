109e3c7c4c7feb0a08bea76ee6242d60dbcb026c
KamilaGrabowska <dudekkamila@icloud.com>
2024-11-22 18:56:26 +0100

    add menu, nagivation and change metadata

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import {Menu} from "@/common/components/Menu";
+
+export const metadata = {
+    title: "Contact next.js app",
+    description: "ths is contact page",
+}
+
+export default function ContactPage () {
+    return (
+        <div>
+            <Menu />
+            <h1>Contact Page</h1>
+        </div>
+    );
+}
--- src/app/contact/team/page.tsx
+++ src/app/contact/team/page.tsx
@@ -1,15 +1,15 @@
 import {Menu} from "@/common/components/Menu";
 
 export const metadata = {
-    title: "Main page next.js app",
-    description: "ths is main page",
+    title: "Team next.js app",
+    description: "ths is team page",
 }
 
-export default function MainPage() {
+export default  function TeamPage () {
     return (
         <div>
             <Menu />
-            <h1>Main Page</h1>
+            <h1>Team Page</h1>
         </div>
     );
 }
